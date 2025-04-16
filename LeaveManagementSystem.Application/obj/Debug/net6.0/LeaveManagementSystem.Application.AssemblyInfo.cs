dd912ea9263ee1f0a7bd0e42f72ab095c5329f93
Houda mahjbi <mahjbihouda@gmail.com>
2025-04-16 14:11:34 +0200

    Add Dtos LeaveRequest

--- LeaveManagementSystem.Application/obj/Debug/net6.0/LeaveManagementSystem.Application.AssemblyInfo.cs
+++ LeaveManagementSystem.Application/obj/Debug/net6.0/LeaveManagementSystem.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeaveManagementSystem.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7340358bc65bf1ec4c50929bd2a55cc45f151ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6752743c111654050dbcf61da2cbe9890b231bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeaveManagementSystem.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeaveManagementSystem.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
